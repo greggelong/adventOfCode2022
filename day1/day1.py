d2822697887097e7c4d499cabf9dad424cb6931b
Greg K <gkreisman_cep@qq.com>
2022-12-04 00:14:32 +0800

    first commit

---
+++ day1/day1.py
@@ -0,0 +1,36 @@
+localpath="C:/Users/gkrei/Documents/adventOfCode2022/day1"
+
+with open(localpath+'/data.txt') as f:
+    lines = f.readlines()
+
+# make the dictionary
+elves ={}
+elf =1
+cals =[]
+for item in lines:
+    elves[elf] = cals
+    if item != '\n':
+        elves[elf].append(int(item))
+    else:
+        elf=elf+1
+        cals =[]
+
+
+#print(elves)
+
+#print(elves.values())
+
+callist = list(elves.values())        
+#print(sum(callist[0]))  
+
+most =0
+myelf =0
+for elf, cal in enumerate(callist):
+    if most < sum(cal):
+        most = sum(cal)
+        myelf = elf
+
+print(myelf+1, most) # +1 because elves start with 1 not zero        
+
+
+   
