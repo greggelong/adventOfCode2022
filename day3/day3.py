048de0d8146bd39545a6d8ff0594da54b45d8a9f
Greg K <gkreisman_cep@qq.com>
2022-12-04 19:05:27 +0800

    day three part 1

--- day3/day3.py
+++ day3/day3.py
@@ -1,6 +1,6 @@
 localpath="C:/Users/gkrei/Documents/adventOfCode2022/day3"
 
-with open(localpath+'/sdata.txt') as f:
+with open(localpath+'/data.txt') as f:
     dsacks = f.readlines()
 
 #import dirty sacks dsacks and then strip \n  to get clean
@@ -8,3 +8,29 @@ with open(localpath+'/sdata.txt') as f:
 sacks = [sack.rstrip('\n') for sack in dsacks]
 #print(dsacks)
 #print(sacks)
+
+## make a lookup table for priorities using a list made from string index of letter is priority
+priorityStr = "0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+priority =list(priorityStr)
+#print(priority)
+#print(priority.index('p'))
+
+total = 0
+for sack in sacks:
+    # split sacks in half
+    sack1s = sack[:-int(len(sack)/2)]
+    sack2s = sack[int(len(sack)/2):]
+    # make into a list
+    sack1 = list(sack1s)
+    sack2 = list(sack2s)
+    ## check for duplicates
+    for item in sack1:
+        if item in sack2:
+            #if in sack to add the priority
+            total= total+ priority.index(item)
+            print(item)
+            break ## only need to find 1
+
+print(total)
+
