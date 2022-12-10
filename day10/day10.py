29ade1a8943728e4adeec88dd556ade2a6cd8ad0
Greg K <gkreisman_cep@qq.com>
2022-12-10 21:38:46 +0800

    day 10 part 1 and part part 2

---
+++ day10/day10.py
@@ -0,0 +1,56 @@
+localpath="C:/Users/gkrei/Documents/adventOfCode2022/day10"
+
+with open(localpath+'/data.txt') as f:
+    dbuffer = f.readlines()
+
+instr = [item.rstrip('\n') for item in dbuffer]
+
+print(instr)
+
+
+def noopit():
+    global cycle
+    cycle+=1
+    if cycle in [20,60,100,140,180,220]:
+            print(cycle,x,cycle*x)
+            signal.append(cycle*x)
+
+
+def addx(strInstr):
+    global x, cycle
+     
+    lstInstr = strInstr.split(' ')
+    #print(int(lstInstr[1]))
+    #take two cycles
+    cycle+=1
+    if cycle in [20,60,100,140,180,220]:
+        print(cycle,x, cycle*x)
+        signal.append(cycle*x)
+        #rangnumb.remove(cycle)
+
+    #after two cycles update x
+   
+    cycle+=1
+    x=x+int(lstInstr[1])
+    if cycle in [20,60,100,140,180,220]:
+        print(cycle,x, cycle*x)
+        signal.append(cycle*x)
+
+
+
+
+x=1
+cycle =1
+
+signal = []
+
+for i in instr:
+    #print(cycle,x)
+    if i[0]=='n':
+        noopit()
+    else:
+        addx(i) 
+
+print(sum(signal))
+
+    
