0cf2ea4d0025b9adb473ba29379c0d2ad3e30ddf
Greg K <gkreisman_cep@qq.com>
2022-12-14 21:02:43 +0800

    change cell connection

--- day12/cell.js
+++ day12/cell.js
@@ -8,12 +8,16 @@ class Cell{
         this.visited = false
         this.start = false
         this.end = false
+        this.parent = null
 
 
     }
-
+/*
     getconnected(){
+
+        // can go lower many levels lower and still be connected
         // check upper neighbor
+        // if it is the same height, or one lower, or one higher it is connected 
         if (this.y-1>=0){
             if (this.h == grid[this.y-1][this.x].h||this.h == grid[this.y-1][this.x].h-1 || this.h == grid[this.y-1][this.x].h+1){
                 this.connected.push(grid[this.y-1][this.x]);
@@ -21,7 +25,7 @@ class Cell{
         }
         // check lower neighbor
         if (this.y+1<=grid.length-1){
-            if (this.h == grid[this.y+1][this.x].h||this.h == grid[this.y+1][this.x].h-1 || this.h == grid[this.y+1][this.x].h+1){
+            if (this.h == grid[this.y+1][this.x].h||this.h ==grid[this.y+1][this.x].h-1 || this.h == grid[this.y+1][this.x].h+1){
                 this.connected.push(grid[this.y+1][this.x]);
             }
         } 
@@ -36,6 +40,39 @@ class Cell{
         // check left neighbor
         if (this.x-1>=0){
             if (this.h == grid[this.y][this.x-1].h||this.h == grid[this.y][this.x-1].h-1 || this.h == grid[this.y][this.x-1].h+1){
+                this.connected.push(grid[this.y][this.x-1]);
+            }
+        } 
+    }
+
+*/
+    getconnected(){
+
+        // can go lower many levels lower and still be connected
+        // check upper neighbor
+        // if this . h is > or equal to neighbor h  or it is one less 
+        if (this.y-1>=0){
+            if (this.h  >= grid[this.y-1][this.x].h  || this.h+1 == grid[this.y-1][this.x].h){
+                this.connected.push(grid[this.y-1][this.x]);
+            }
+        }
+        // check lower neighbor
+        if (this.y+1<=grid.length-1){
+            if (this.h >= grid[this.y+1][this.x].h|| this.h+1 == grid[this.y+1][this.x].h){
+                this.connected.push(grid[this.y+1][this.x]);
+            }
+        } 
+
+        // check right neighbor
+        if (this.x+1<=grid[0].length-1){
+            if (this.h >=grid[this.y][this.x+1].h||this.h+1 == grid[this.y][this.x+1].h){
+                this.connected.push(grid[this.y][this.x+1]);
+            }
+        } 
+
+        // check left neighbor
+        if (this.x-1>=0){
+            if (this.h >= grid[this.y][this.x-1].h|| this.h+1 == grid[this.y][this.x-1].h){
                 this.connected.push(grid[this.y][this.x-1]);
             }
         } 
